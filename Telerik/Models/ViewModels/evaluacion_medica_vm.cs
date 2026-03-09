816c6793840c9a0ed5248fc32a93e49bec300ab0
Rcarlohh <rangel.crlos@gmail.com>
2026-03-09 08:17:12 -0600

    Eliminacion de archivos basura

--- Telerik/Models/ViewModels/evaluacion_medica_vm.cs
+++
@@ -1,7 +0,0 @@
-// Este archivo fue vaciado intencionalmente.
-// Las clases de EvaluacionMedicaVm y sub-clases estaban duplicadas en dos carpetas:
-//   - Models/ViewModels/evaluacion_medica_vm.cs  (ESTE ARCHIVO - ya no se usa)
-//   - Models/view_models/evaluacion_medica_vm.cs (VERSION ACTIVA - tiene NombreCandidato, etc.)
-//
-// Clase real en: Models/view_models/evaluacion_medica_vm.cs
-// Namespace: Telerik.Models.ViewModels
